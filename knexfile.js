5815254f6970fc556c5192b0d5a50c25828eaac9
fahtulabdulrahman@gmail.com <fahtulabdulrahman@gmail.com>
2026-01-03 19:39:58 +0800

    change knex

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,55 @@
+// require("dotenv").config();
+
+// module.exports = {
+//   development: {
+//     client: process.env.DB_CLIENT || "pg",
+//     connection: {
+//       host: process.env.DB_HOST,
+//       port: Number(process.env.DB_PORT || 5432),
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//     },
+//     migrations: { directory: "./migrations" },
+//     seeds: { directory: "./seeds" },
+//   },
+//   production: {
+//     client: process.env.DB_CLIENT || "pg",
+//     connection: {
+//       host: process.env.DB_HOST,
+//       port: Number(process.env.DB_PORT || 5432),
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       ssl: { rejectUnauthorized: false },
+//     },
+//     migrations: { directory: "./migrations" },
+//   },
+// };
+
 require("dotenv").config();
+
+const isLocal = ["127.0.0.1", "localhost"].includes(process.env.DB_HOST);
+
+const connection = {
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 5432),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: isLocal ? false : { rejectUnauthorized: false },
+};
 
 module.exports = {
   development: {
     client: process.env.DB_CLIENT || "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT || 5432),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    connection,
     migrations: { directory: "./migrations" },
     seeds: { directory: "./seeds" },
   },
   production: {
     client: process.env.DB_CLIENT || "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT || 5432),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: { rejectUnauthorized: false },
-    },
+    connection,
     migrations: { directory: "./migrations" },
   },
 };
